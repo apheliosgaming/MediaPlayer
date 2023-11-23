a7edeb36cb2c2029adf4f014975b5a6b689a7f48
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-22 22:40:00 -0300

    Update Usuario.java

--- MediaPlayer/src/main/java/br/ufrn/imd/models/Usuario.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/models/Usuario.java
@@ -1,4 +1,25 @@
 package br.ufrn.imd.models;
 
 public class Usuario {
+  private String email;
+  private int senha;
+    
+    public UsuarioVIP(double codigo){
+        super();
+    }
+    
+    public String getEmail() {
+    
+  }
+
+    public void setEmail(String email) {
+
+  }
+    public int getSenha() {
+    
+  }
+
+    public void setSenha(int senha) {
+
+  }
 }

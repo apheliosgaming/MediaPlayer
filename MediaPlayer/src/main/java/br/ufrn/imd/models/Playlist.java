17b7cf8ecf75a389cd415848735272fea34e7fcf
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-22 23:19:53 -0300

    Update Playlist.java

--- MediaPlayer/src/main/java/br/ufrn/imd/models/Playlist.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/models/Playlist.java
@@ -1,1 +1,35 @@
+package br.ufrn.imd.models;
 
+import java.util.ArrayList;
+
+public class Playlist {
+    private String nome;
+    private ArrayList<Musica> musicas;
+    
+    public Playlist(String nome, ) {
+        this.nome = nome;
+        this.musicas = new ArrayList<Musica>();
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+     public ArrayList<Musica> getMusicas() {
+        return this.musicas;
+    }
+
+    public void addMusica(Musica musica) {
+        this.musicas.add(musica);
+    }
+    
+    public void addMusica(Musica musica) {
+        this.musicas.add(musica);
+    }
+    public void removeMusica(Musica musica){
+    }
+}

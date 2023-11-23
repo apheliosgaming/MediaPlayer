5888fb214cf23d6292eb6b58d756f5add4293122
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-22 22:28:21 -0300

    Update UsuarioVIP.java

--- MediaPlayer/src/main/java/br/ufrn/imd/models/UsuarioVIP.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/models/UsuarioVIP.java
@@ -1,1 +1,19 @@
+package br.ufrn.imd.models;
 
+public class UsuarioVIP extends Usuario{
+    private double codigo;
+    
+    public UsuarioVIP(double codigo){
+        super();
+        this.codigo = codigo;
+    }
+    
+    public double getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(double codigo) {
+    this.codigo = codigo;
+  }
+    
+}

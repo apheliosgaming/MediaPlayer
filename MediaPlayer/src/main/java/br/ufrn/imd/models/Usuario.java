ab0bb0ad58e319543f70952731b89b31b4bcce6f
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-22 23:33:50 -0300

    Update Usuario.java

--- MediaPlayer/src/main/java/br/ufrn/imd/models/Usuario.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/models/Usuario.java
@@ -9,17 +9,17 @@ public class Usuario {
     }
     
     public String getEmail() {
-    
+        return email;
   }
 
     public void setEmail(String email) {
-
+        this.email = email;
   }
     public int getSenha() {
-    
+        return senha;
   }
 
     public void setSenha(int senha) {
-
+        this.senha = senha;
   }
 }

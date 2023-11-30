c64c966c0550cd2625d0d87143ae9f072faa9a97
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-30 01:16:11 -0300

    Update LoginController.java

--- MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
@@ -1,15 +1,43 @@
-package app;
+package br.ufrn.imd.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+
+//import java.sql.Connection;
+
+//import java.awt.event.ActionEvent;
 
 public class LoginController {
 
     @FXML
-    private Button ConfirmButtom;
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private Button ConfirmButton;
+
+    @FXML
+    private Label WarningTextLogin;
+
+    public void ConfirmButtonOnAction(){
+        if(!usernameField.getText().isBlank() && !passwordField.getText().isBlank()){
+            WarningTextLogin.setText("Você logou!");
+            validaLogin();
+        }
+        else{
+            WarningTextLogin.setText("Insira um usuário e senha.");
+        }
+    }
+    public void validaLogin(){
+        //parte do UsuarioDAO boto amanha XD
+    }
 
     @FXML
     private ImageView GuyHeadphonesLogin;
@@ -18,12 +46,8 @@ public class LoginController {
     private ImageView IconHeadPhone;
 
     @FXML
-    private Button RegisterButtom;
+    private Button RegisterButton;
 
-    @FXML
-    private PasswordField passwordField;
-
-    @FXML
-    private TextField usernameField;
+    public void RegisterButtonOnAction(ActionEvent e){}
 
 }

5dea730f785af2fadf0ca5e999b56e15b298f31c
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-29 23:03:55 -0300

    Update LoginController.java

--- MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
@@ -1,1 +1,29 @@
+package app;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginController {
+
+    @FXML
+    private Button ConfirmButtom;
+
+    @FXML
+    private ImageView GuyHeadphonesLogin;
+
+    @FXML
+    private ImageView IconHeadPhone;
+
+    @FXML
+    private Button RegisterButtom;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+}

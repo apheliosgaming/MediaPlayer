a7b16125bbc98d8a6b376221b3a88a39d34f3ab2
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-30 23:56:10 -0300

    Update LoginController.java

--- MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/controllers/LoginController.java
@@ -1,18 +1,29 @@
 package br.ufrn.imd.controllers;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 //import java.sql.Connection;
 
 //import java.awt.event.ActionEvent;
 
-public class LoginController {
+public class LoginController implements Initializable {
 
     @FXML
     private PasswordField passwordField;
@@ -36,6 +47,7 @@ public class LoginController {
         }
     }
     public void validaLogin(){
+        //Connection connection;
         //parte do UsuarioDAO boto amanha XD
     }
 
@@ -48,6 +60,26 @@ public class LoginController {
     @FXML
     private Button RegisterButton;
 
-    public void RegisterButtonOnAction(ActionEvent e){}
 
-}
+    public void RegisterButtonOnAction(ActionEvent e){
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        RegisterButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("RegistrationPage.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.setTitle("Tela de Registro");
+                stage.setScene(new Scene(root, 520, 400));
+                stage.show();
+            }
+        });
+    }}

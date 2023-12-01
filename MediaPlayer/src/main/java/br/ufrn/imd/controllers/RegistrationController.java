ace5342d5f22d0ee191baef0c66b51f9ef386c4a
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-11-30 23:56:54 -0300

    Update RegistrationController.java

--- MediaPlayer/src/main/java/br/ufrn/imd/controllers/RegistrationController.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/controllers/RegistrationController.java
@@ -1,18 +1,29 @@
 package app;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 //import java.sql.Connection;
 
 //import java.awt.event.ActionEvent;
 
-public class RegistrationController {
+public class RegistrationController implements Initializable {
 
     @FXML
     private Button ConfirmButton;
@@ -48,4 +59,23 @@ public class RegistrationController {
         }
     }
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        LoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.setTitle("Tela de Login");
+                stage.setScene(new Scene(root, 520, 400));
+                stage.show();
+            }
+        });
+    }
 }

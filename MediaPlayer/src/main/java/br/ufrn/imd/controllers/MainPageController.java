ed0f0c8d1a067d637d5048a1a8ad5ad2f397ef5a
ian <108193413+apheliosgaming@users.noreply.github.com>
2023-12-06 02:16:28 -0300

    Update MainPageController.java
    musica e controles do player funcionando

--- MediaPlayer/src/main/java/br/ufrn/imd/controllers/MainPageController.java
+++ MediaPlayer/src/main/java/br/ufrn/imd/controllers/MainPageController.java
@@ -1,28 +1,35 @@
 package br.ufrn.imd.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+//import javafx.scene.media.MediaView;
+
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
-
-import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.File;
+
+import java.util.*;
 
 public class MainPageController implements Initializable {
 
 
     @FXML
     private Button addDirectory;
+
+    @FXML
+    private Button pauseButton;
 
     @FXML
     private Button addFile;
@@ -46,16 +53,19 @@ public class MainPageController implements Initializable {
     private AnchorPane player;
 
     @FXML
-    private ComboBox<?> speedBox;
+    private ComboBox speedBox;
 
     @FXML
     private Slider volumeSlider;
 
     @FXML
     private Label welcomeLabel;
-
     @FXML
-    private MediaView mediaView;
+    private ListView<String> musicList;
+    @FXML
+    private ListView<String> playlistList;
+    @FXML
+    private ImageView imagePause;
 
     private File directory;
     private File[] files;
@@ -63,7 +73,6 @@ public class MainPageController implements Initializable {
     private ArrayList<File> songs;
 
     private int songNumber;
-    private int[] speeds = {25, 50, 100, 125, 150, 200};
 
     private Timer timer;
     private TimerTask task;
@@ -71,10 +80,45 @@ public class MainPageController implements Initializable {
     private String path;
     private MediaPlayer mediaPlayer;
     private Media media;
+    private String songName;
 
 
     public void changeSpeed(ActionEvent event) {
+        String velocidade = speedBox.getSelectionModel().getSelectedItem().toString();
+        if(Objects.equals(velocidade, "50%")){
+            mediaPlayer.setRate(50*0.01);
+        }
+        if(Objects.equals(velocidade, "75%")){
+            mediaPlayer.setRate(75*0.01);
+        }
+        if(Objects.equals(velocidade, "100%")){
+            mediaPlayer.setRate(100*0.01);
+        }
+        if(Objects.equals(velocidade, "125%")){
+            mediaPlayer.setRate(125*0.01);
+        }
+        if(Objects.equals(velocidade, "150%")){
+            mediaPlayer.setRate(150*0.01);
+        }
+        if(Objects.equals(velocidade, "200%")){
+            mediaPlayer.setRate(200*0.01);
+        }
 
+    }
+
+    public void MudaAvatarOnAction(){
+        /*
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecione seu novo avatar");
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null) {
+            path = file.toURI().toString();
+            System.out.println(path);
+        }
+        else{
+            System.out.println("Erro!");
+        }
+        */
     }
 
 
@@ -84,6 +128,11 @@ public class MainPageController implements Initializable {
 
 
     public void playMedia(ActionEvent event) {
+        mediaPlayer.play();
+    }
+    public void pauseMedia(ActionEvent event) {
+        mediaPlayer.pause();
+        //imagePause.setImage(new Image("java/resources/util_RESOURCES/play(2).png"));
 
     }
 
@@ -97,23 +146,27 @@ public class MainPageController implements Initializable {
     public void cancelTimer(){
 
     }
+    @FXML
     public void AddFileMethod(ActionEvent actionEvent){
         FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Selecione o arquivo da música");
         File file = fileChooser.showOpenDialog(null);
-        path = file.toURI().toString();
-        if(path != null){
-            try {
-                media = new Media(path);
-                mediaPlayer = new MediaPlayer(media);
-                mediaPlayer.play();
-                mediaPlayer.setVolume(0.5);
-            } catch (Exception e){
-                e.printStackTrace();
+        if(file != null){
+            path = file.toURI().toString();
+            System.out.println(path);
+            media = new Media(path);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.play();
+
+
+
+            //} catch (Exception e){
+             //   e.printStackTrace();
+           // }
             }
-        }
         else{
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("ALGO NO PROCESSO DEU ERRADO.");
+            alert.setContentText("O arquivo fornecido é vazio, incompatível ou inexistente.");
             alert.show();
         }
     }
@@ -126,9 +179,36 @@ public class MainPageController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       // songs = new ArrayList<File>();
+        ObservableList<String> list = FXCollections.observableArrayList(Integer.toString(50) + "%",
+                Integer.toString(75) + "%", Integer.toString(100) + "%",
+                Integer.toString(125) + "%", Integer.toString(150) + "%",
+                Integer.toString(200) + "%");
+        speedBox.setItems(list);
 
-        //directory = new File();
+        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                mediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+            }
+        });
+        //musicList.getItems().addAll("cachorro", "gato.mp3");
+        //playlistList.getItems().addAll("a", "0");
+
+
+
+      // songs = new ArrayList<File>();
+
+      // directory = new File("util");
+
+      // files = directory.listFiles();
+
+     //  if(files != null){
+         //  for(File file : files){
+          //     songs.add(file);
+           //    System.out.println(file);
+         //  }
+     //  }
     }
-}
+  //  Media media = new Media(songs.get(songNumber));
 
+}
